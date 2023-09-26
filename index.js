04279eab01e95d42e1e2da47238905569a2ba1e1
DOOM097 <98549250+DOOM097@users.noreply.github.com>
2023-09-26 15:04:29 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,84 +1,70 @@
-// Модуль для работы с файловой системой Node.js
 const fs = require('fs');
 
-// Подключаем настройки базы данных Sequelize из файла database.js
 const sequelize = require('./config/database');
 
-// Подключаем модели данных (таблицы)
+// Подключаем модели
 const Book = require('./models/book');
 const Author = require('./models/author');
 const Category = require('./models/category');
 
-// Путь к JSON-файлу с данными для заполнения базы данных
+// Путь к JSON
 const jsonDataPath = './books.json';
 
 // Определение связи многие-ко-многим между моделями
 Book.belongsToMany(Author, { through: 'BookAuthor', timestamps: false });
-
 Book.belongsToMany(Category, { through: 'BookCategory', timestamps: false });
 Author.belongsToMany(Book, { through: 'BookAuthor', timestamps: false });
 Category.belongsToMany(Book, { through: 'BookCategory', timestamps: false });
 
-// Вызов sync() для создания таблиц и связей в базе данных
-
-// Синхронизация моделей с базой данных. Это создаст таблицы, если их нет.
-sequelize.sync()
+//sync() для создания таблиц и связей в базе данных
+  sequelize.sync()
   .then(async () => {
-    // Чтение данных из JSON-файла
-
-    // Чтение содержимого JSON-файла с данными. Если произойдет ошибка, будет выведено сообщение об ошибке.
+      //Вместо ожидания завершения операции, асинхронная функция обратного вызова позволяет продолжить выполнение остальной части кода.
+      //data - это параметр функции обратного вызова, который будет содержать содержимое файла после успешного чтения
     fs.readFile(jsonDataPath, 'utf8', async (err, data) => {
       if (err) {
-        console.error('Ошибка при чтении JSON-файла:', err);
+        console.error('Ошибка при чтении JSON:', err);
         return;
       }
-
-      // Разбор JSON-данных в объект
       const jsonData = JSON.parse(data);
-
       try {
-
-        // Создание массива данных для модели Book, исключая поля authors и categories из каждого элемента данных.
-        const booksData = jsonData.map((item) => {
-          const { authors, categories, ...bookData } = item;
-          return bookData;
+            // Создание массива данных для модели Book
+            //...bookData - это оператор "распыления" (spread operator), который собирает все остальные свойства объекта item и сохраняет их в переменной bookData в виде объекта. 
+            const booksData = jsonData.map((item) => {
+            const { authors, categories, ...bookData } = item;
+            return bookData;
         });
 
-        // Создание массивов данных для моделей Author и Category, уникальных значений имен авторов и категорий.
-        const authorsData = [...new Set(jsonData.flatMap((item) => item.authors))];
-        const categoriesData = [...new Set(jsonData.flatMap((item) => item.categories))];
+          // Создание массивов данных для моделей Author и Category
+          //.flatMap() для извлечения значений authors и categories из объектов JSON
+          const authorsData = [...new Set(jsonData.flatMap((item) => item.authors))];
+          const categoriesData = [...new Set(jsonData.flatMap((item) => item.categories))];
+        
+          
+          //await для массовой вставки (или создания) записей в базе данных
+          //bulkCreate метод который позволяет вставлять множество записей в базу данных сразу.
+          //await перед Book.bulkCreate, вы ждете завершения операции вставки данных в базу данных, прежде чем перейти к следующим действиям
+          await Book.bulkCreate(booksData);
+          //если authorsData содержит ['Автор1', 'Автор2'], то после .map() получится массив объектов [{ name: 'Автор1' }, { name: 'Автор2' }]
+          await Author.bulkCreate(authorsData.map((name) => ({ name })));
+          await Category.bulkCreate(categoriesData.map((name) => ({ name })));
 
+          // Проходим по каждому элементу данных в JSON и устанавливаем связи для каждой книги.
+          for (const item of jsonData) {
 
-        // Создание записей в таблице Book на основе массива booksData.
-        await Book.bulkCreate(booksData);
-
-        // Создание записей в таблице Author на основе массива authorsData, с преобразованием имен в объекты.
-        await Author.bulkCreate(authorsData.map((name) => ({ name })));
-
-        // Создание записей в таблице Category на основе массива categoriesData, с преобразованием имен в объекты.
-        await Category.bulkCreate(categoriesData.map((name) => ({ name })));
-
-        // Установка связей между книгами, авторами и категориями
-
-        // Проходим по каждому элементу данных в JSON и устанавливаем связи для каждой книги.
-        for (const item of jsonData) {
-          // Находим книгу в таблице Book по названию.
+          // Находим книгу, авторов, категории в таблицах
           const book = await Book.findOne({ where: { title: item.title } });
-
-          // Находим авторов в таблице Author по именам из данных.
           const bookAuthors = await Author.findAll({ where: { name: item.authors } });
-
-          // Находим категории в таблице Category по именам из данных.
           const bookCategories = await Category.findAll({ where: { name: item.categories } });
 
-          // Устанавливаем связи между книгой, авторами и категориями.
+          //bookAuthors представляет массив авторов, которые мы связываем с данной книгой.
           await book.setAuthors(bookAuthors);
           await book.setCategories(bookCategories);
         }
 
-        console.log('Данные успешно добавлены в базу данных');
+        console.log('Данные добавлены!');
       } catch (error) {
-        console.error('Ошибка при добавлении данных в базу данных:', error);
+        console.error('Ошибка при добавлении данных:', error);
       } finally {
 
         await sequelize.close();
@@ -86,5 +72,5 @@ sequelize.sync()
     });
   })
   .catch((error) => {
-    console.error('Ошибка при синхронизации базы данных и создании связей:', error);
+    console.error('Ошибка при синхронизации:', error);
   });
